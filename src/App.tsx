6a3a02d19e7a42123613bea3523fecf4b21474f3
Pedro Furlan <pedrofurlan6@gmail.com>
2023-05-08 15:38:22 -0300

    Add images on assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
-import "./App.css";
 import { GlobalStyle } from "./styles/global";
 import lightTheme from "./themes/lightTheme";
+import darkTheme from "./themes/darkTheme";
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
+      <div>
+        <h1>OLÁ MUNDO</h1>
+      </div>
       <GlobalStyle />
     </ThemeProvider>
   );
