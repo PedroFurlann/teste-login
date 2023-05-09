5da2cdb50ffbd144c09ac9cd3f79967b8ef01d87
Pedro Furlan <pedrofurlan6@gmail.com>
2023-05-08 21:14:42 -0300

    Create function to get the inputs values

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -15,11 +15,23 @@ import {
 } from "./styles";
 import login_img from "../../assets/login_img.jpeg";
 import menu_img from "../../assets/logo_waybe.jpeg";
-import footer_img from '../../assets/logo_sifat_light.jpeg'
+import footer_img from "../../assets/logo_sifat_light.jpeg";
 import { TitleAndSubtitle } from "../../components/TitleAndSubtitle";
 import { Button } from "../../components/Button";
+import { ChangeEvent, useState } from "react";
 
 export function Login() {
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
+
+  function handleUserChange(event: ChangeEvent<HTMLInputElement>) {
+    setUser(event.target.value);
+  }
+
+  function handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value);
+  }
+
   return (
     <Container>
       <LoginImgContainer src={login_img} />
@@ -32,35 +44,34 @@ export function Login() {
           />
         </HeaderMenuContainer>
         <FormContainer>
-          
-          <InputTextContainer type="text" placeholder="Email"  />
+          <InputTextContainer
+            type="text"
+            placeholder="Email"
+            onChange={handleUserChange}
+            required
+          />
           <hr style={{ marginBottom: 16 }} />
-          <InputTextContainer type="password" placeholder="Senha" />
+          <InputTextContainer
+            type="password"
+            placeholder="Senha"
+            onChange={handlePasswordChange}
+            required
+          />
           <hr style={{ marginBottom: 24 }} />
-          
 
-          
           <CheckBoxContainer>
             <InputCheckBoxContainer type="checkbox" />
-            <LabelCheckBoxContainer>
-              Lembrar usuário
-            </LabelCheckBoxContainer>
+            <LabelCheckBoxContainer>Lembrar usuário</LabelCheckBoxContainer>
           </CheckBoxContainer>
-          
 
-        <ButtonAndTextContainer>
-          <Button type="submit" text="Entrar" style={{ marginBottom: 16 }} />
-          <TextContainer>
-            Esqueci minha senha
-          </TextContainer>
-        </ButtonAndTextContainer>    
-        
+          <ButtonAndTextContainer>
+            <Button type="submit" text="Entrar" style={{ marginBottom: 16 }} />
+            <TextContainer>Esqueci minha senha</TextContainer>
+          </ButtonAndTextContainer>
         </FormContainer>
 
-
-        
         <FooterContainer>
-          <img src={footer_img} alt=""/>
+          <img src={footer_img} alt="" />
         </FooterContainer>
       </MenuContainer>
     </Container>
